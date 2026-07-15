da9211bd6c6e219e40c633561b150689553e5c3e
Daniel <dangcasanova@gmail.com>
2026-07-15 09:11:39 -0300

    Avoid plasTeX context recursion

--- scripts/plastex_gerby.py
+++ scripts/plastex_gerby.py
@@ -4,6 +4,8 @@ import runpy
 import shutil
 import sys
 
+from plasTeX.Context import ContextItem
+
 try:
   import jinja2
 except ImportError:
@@ -13,11 +15,31 @@ else:
     jinja2.contextfunction = jinja2.pass_context
 
 
+def context_item_getitem(context, key):
+  seen = set()
+  while context is not None:
+    identifier = id(context)
+    if identifier in seen:
+      break
+    seen.add(identifier)
+
+    try:
+      return dict.__getitem__(context, key)
+    except KeyError:
+      parent = context.parent
+      if parent is context:
+        break
+      context = parent
+
+  raise KeyError(key)
+
+
 def main():
   plastex = shutil.which("plastex")
   if plastex is None:
     raise SystemExit("plastex executable not found")
 
+  ContextItem.__getitem__ = context_item_getitem
   sys.argv[0] = plastex
   runpy.run_path(plastex, run_name="__main__")
 
